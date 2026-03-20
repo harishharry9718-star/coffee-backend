6398026370665a3c80fa7b093bdf98b37df42d61
harishharry9718-star <harishhary.9718@gmail.com>
2026-03-20 12:08:25 +0530

    Fix CORS issue

--- src/main/java/com/example/coffeeshop/config/CorsConfig.java
+++ src/main/java/com/example/coffeeshop/config/CorsConfig.java
@@ -2,8 +2,7 @@ package com.example.coffeeshop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
@@ -11,15 +10,13 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("*")
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*");
-
             }
         };
     }
